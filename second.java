7e174cd60894835aac307008328a42720357c5d7
Rutikkobarne01 <rutikkobarne@gmail.com>
2024-11-14 23:07:30 +0530

    assignment

---
+++ second.java
@@ -0,0 +1,11 @@
+public class second {
+    public static void main(String[]args) {
+        int a=35;
+        int b=55;
+        int c=99;
+        int sum=(a-b/c);
+
+        System.out.println(sum);
+
+    }
+}
